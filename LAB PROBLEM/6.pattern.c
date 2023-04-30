fa08a18ec0f65a971396906ebbe7bc4aae3ed1da
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-30 22:10:43 +0600

    c

--- LAB PROBLEM/6.pattern.c
+++ LAB PROBLEM/6.pattern.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("%d",&n);
-    for(int i=0;i<n;i++)
+    int n,p,j,k;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
     {
-        cin>>ar[i];
+        for(j=i;j<i*2;j++)
+        {
+            printf("%d ",j);
+        }
+        for(k=j-2;k>=i;k--)
+        {
+            printf("%d ",k);
+        }
+         printf("\n");
     }
     return 0;
 }
