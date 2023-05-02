07ead7d917675f2dd0aba01e3dedb811d1a350b6
mehedinaeem <mehedinaeem00@gmail.com>
2023-05-02 23:26:28 +0600

    c

--- LAB PROBLEM/8.pyramid_of_star.c
+++ LAB PROBLEM/8.pyramid_of_star.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-int main()
-{
+
+int main() {
     int n;
+
     scanf("%d", &n);
-    
-    //row
-    for (int i = n; i > 0; i--)
+
+    for (int i = n; i >= 1; i--) 
     {
-        //column
-        for (int j = i; j > 0; j--)
-        {
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (int k = 1; k <= 2 * i - 1; k++)
             printf("*");
-        }
+
         printf("\n");
     }
 
