7277ceb74be00369b5702bac112bf245868cd621
mehedinaeem <mehedinaeem00@gmail.com>
2023-05-09 18:20:33 +0600

    c

--- 3.DEVOLOP LAB PROBLEM/9.mat.cpp
+++ 3.DEVOLOP LAB PROBLEM/9.mat.cpp
@@ -20,37 +20,17 @@ void addMatrices(int A[][100], int B[][100], int C[][100], int x, int y)
         {
             C[i][j] = A[i][j] + B[i][j];
         }
-        
-    }
-    cout << "The sum of the matrices is:" << endl;
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            cout << C[i][j] << " ";
-        }
-        cout << endl;
     }
 }
 
-int subMatrix(int A[][100], int B[][100], int C[][100], int x, int y)
+void subMatrices(int A[][100], int B[][100], int C[][100], int x, int y)
 {
-     for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            C[i][j] = B[i][j]-A[i][j];
-        }
-        
-    }
-    cout << "The sub of the matrices is:" << endl;
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
-            cout << C[i][j] << " ";
+            C[i][j] = A[i][j] - B[i][j];
         }
-        cout << endl;
     }
 }
 
@@ -71,11 +51,12 @@ int main()
     int t, r, c;
     cout << "Enter how many matrices you want to add: ";
     cin >> t;
+    int A[100][100], B[100][100], C[100][100]; // declare matrices outside the loop
     for (int i = 1; i <= t; i++)
     {
         cout << "Enter the number of rows and columns of matrix " << i << ": ";
         cin >> r >> c;
-        int A[100][100], B[100][100], C[100][100];
+
         cout << "Enter the elements of matrix " << i << ":" << endl;
         getMatrixElements(A, r, c);
 
@@ -99,9 +80,8 @@ int main()
             addMatrices(A, C, C, r, c);
         }
 
-
-        //sub matrix
-         if (i == 1)
+        //SUBTRACT Matrix
+        if (i == 1)
         {
             for (int p = 0; p < r; p++)
             {
@@ -113,7 +93,16 @@ int main()
         }
         else
         {
-            subMatrix(B, A, C, r, c);
+            subMatrices(B, A, B, r, c);
+        }
+
+        // display sum and difference
+        if (i == t)
+        {
+            cout << "The final sum of the matrices is:" << endl;
+            displayMatrix(C, r, c);
+            cout << "The final difference of the matrices is:" << endl;
+            displayMatrix(B, r, c);
         }
     }
     return 0;
