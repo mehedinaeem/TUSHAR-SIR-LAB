3cbc06c7eb2ba12e7c5958f33f3a3dfaa51a03ad
mehedinaeem <mehedinaeem00@gmail.com>
2023-05-26 12:02:48 +0600

    ASSIGNMENT

---
+++ 4.Theory Assignment/9.17_lagrge_small.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+int main()
+{
+    int n;
+    char s[100];
+    printf("Enter the number of strings: ");
+    scanf("%d", &n);
+    int ar[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+
+        scanf("%s", s);
+        
+        int j = 0;
+        int length = 0;
+        
+        while (s[j] != '\0')
+        {
+            length++;
+            j++;
+        }
+        
+        ar[i] = length;
+    }
+
+    int max = INT_MIN;
+    int min = INT_MAX;
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (ar[i] > max)
+            max = ar[i];
+        if (ar[i] < min)
+            min = ar[i];
+    }
+    
+    printf("Max length: %d\n", max);
+    printf("Min length: %d\n", min);
+
+    return 0;
+}
