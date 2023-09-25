9c2cadefaa705eaa1f8268d6e6f6caf4fe0f1bb9
mehedinaeem <mehedinaeem00@gmail.com>
2023-09-25 08:37:58 +0600

    mark

---
+++ Kamal Sir Lab/mark.py
@@ -0,0 +1,12 @@
+n=int(input("Enter student Number:"))
+mark=[]
+for i in range(n):
+    print(f'For student {i+1}:')
+    python=int(input("Python mark:"))
+    python_lab=int(input("Python lab mark:"))
+    print('\n')
+    total_mark=python+python_lab
+    mark.append(total_mark)
+    
+for i in range(n):
+    print(f'Student {i+1} total mark is {mark[i]}')
