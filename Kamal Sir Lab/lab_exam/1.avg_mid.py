a421b980ec70c7ab4a68c6f9300b1892dff82112
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-11 22:08:06 +0600

    mid number

---
+++ Kamal Sir Lab/lab_exam/1.avg_mid.py
@@ -0,0 +1,8 @@
+def mid_avg():
+    mid1=int(input("Enter mid 1 number:"))
+    mid2=int(input("Enter mid 2 number:"))
+    mid3=int(input("Enter mid 3 number:"))
+    avg=(mid1+mid2+mid3)/3
+    print(avg)
+    
+mid_avg()
