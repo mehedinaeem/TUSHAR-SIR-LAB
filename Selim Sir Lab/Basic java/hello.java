61f12f85bc37aeacc31ac8ff7547f1987e4df35d
mehedinaeem <mehedinaeem00@gmail.com>
2024-01-17 10:02:56 +0600

    java

---
+++ Selim Sir Lab/Basic java/hello.java
@@ -0,0 +1,41 @@
+// public class 1.hello {
+//     public static void main(String[] args) {
+//         System.out.print("Hello world!");
+//     }
+// }
+// public class hello {
+//     public static void main(String[] args) {
+//       System.out.println("Hello World");
+//     }
+//   }
+
+/**
+ * hello
+ */
+import java.util.Scanner;
+
+public class hello {
+  public static void main(String[] args) {
+
+    // make an object under scanner
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter something: ");
+
+    String userinput = scanner.nextLine();
+
+    System.out.println("Your output: " + userinput);
+
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.print("hello world\n");
+    System.out.println(2 * 3);
+
+  }
+
+}
