cdbcbf4ffb51cd75a2143b4a45fc3edf153a5671
mehedinaeem <mehedinaeem00@gmail.com>
2024-01-24 09:57:21 +0600

    java

---
+++ Selim Sir Lab/Basic java/input.java
@@ -0,0 +1,25 @@
+// import java.util.Scanner;
+
+// public class input {
+//     public static void main(String[] Args){
+//         Scanner inp=new Scanner(System.in);
+//         int num1=inp.nextInt();
+//         int num2=inp.nextInt();
+
+//         int sum=num1+num2;
+//         System.out.println("The sum is: "+ sum);
+//     }
+// }
+
+
+import java.util.Scanner;
+
+public class input{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int num1=in.nextInt();
+        int num2=in.nextInt();
+
+        System.out.print(num1+" "+num2);
+    }
+}
